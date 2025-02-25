19d28a405102a7edee87200c62a51a6fa358adde
liledix4 <liledix4@musician.org>
2025-02-25 23:06:29 +0000

    0.1.0

--- gears/js/main.js
+++ gears/js/main.js
@@ -3,6 +3,9 @@ import { queryString } from "../modules/js_url/url.min.js";
 
 console.log(await queryString().then(queryStringObject => {
   if (queryStringObject.v) {
-    return queryStringObject.v;
+    const video = queryStringObject.v;
+    if ( video.match(/^[0-9A-Za-z_\-]{11}$/).length <= 0 )
+      return;
+    document.querySelector('body').innerHTML = `<iframe src="https://www.youtube.com/embed/${video}" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
   }
 }));
